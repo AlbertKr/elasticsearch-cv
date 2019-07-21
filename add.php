30c4870f955eddee14bf8be3cc57380f80913deb
Albert <krasniqi.albert24@gmail.com>
2019-07-21 20:17:06 +0200

    ajout interface, manque visuel

--- add.php
+++ add.php
@@ -3,24 +3,30 @@
 require_once 'app/init.php';
 
 if (!empty($_POST)) {
-    if (isset($_POST['title'], $_POST['body'], $_POST['keywords'])) {
-        $title = $_POST['title'];
-        $body = $_POST['body'];
-        $keywords = explode(',', $_POST['keywords']);
+    if (isset($_POST['name'], $_POST['firstname'], $_POST['diplomas'], $_POST['experience'], $_POST['competence'], $_POST['hobbies'])) {
+        $name = $_POST['name'];
+        $firstname = $_POST['firstname'];
+        $diplomas = explode(';', $_POST['diplomas']);
+        $experience = explode(';', $_POST['experience']);
+        $competence = explode(';', $_POST['competence']);
+        $hobbies = explode(';', $_POST['hobbies']);
 
         $indexed = $es->index([
-            'index' => 'articles',
-            'type' => 'article',
+            'index' => 'cvs',
+            'type' => 'cv',
             'body' => [
-                'title' => $title,
-                'body' => $body,
-                'keywords' => $keywords
+                'name' => $name,
+                'firstname' => $firstname,
+                'diplomas' => $diplomas,
+                'experience' => $experience,
+                'competence' => $competence,
+                'hobbies' => $hobbies
             ]
         ]);
 
-        if ($indexed) {
-            print_r($indexed);
-        }
+        // if ($indexed) {
+        //     print_r($indexed);
+        // }
     }
 }
 
@@ -31,28 +37,62 @@ if (!empty($_POST)) {
 
 <head>
     <meta charset="utf-8">
-    <title>Ajouter Doc</title>
+    <title>Ajouter un CV</title>
 
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/add.css">
 </head>
 
 <body>
-    <form action="add.php" method="post" autocomplete="off">
-        <label>
-            Titre
-            <input type="text" name="title">
-        </label>
-        <label>
-            Contenu
-            <textarea name="body" row="8"></textarea>
-        </label>
-        <label>
-            Keywords
-            <input type="text" name="keywords" placeholder="comma, separate">
-        </label>
+    <div id="div_header">
+        <h1>Ajoutez un CV !</h1>
+        <a href="index.php">
+            <button> Accéder à la recherche de CV </button>
+        </a>
+    </div>
 
-        <input type="submit" value="Add">
-    </form>
+    <div id="div_global">
+        <div id="div_form">
+            <h2>Via le Formulaire </h2>
+            <form action="add.php" method="post" autocomplete="off" class="form-add">
+                <label>
+                    <p>Nom : </p>
+                    <input type="text" name="name">
+                </label>
+                <label>
+                    <p>Prénom : </p>
+                    <input type="text" name="firstname">
+                </label>
+                <label>
+                    <p>Diplômes : </p>
+                    <textarea name="diplomas" row="8" placeholder="Renseignez vos diplômes. (Séparées par des points virgules (;))"></textarea>
+                </label>
+                <label>
+                    <p>Expériences : </p>
+                    <textarea name="experience" row="8" placeholder="Décrivez vos différentes expériences. (Séparées par des points virgules (;))"></textarea>
+                </label>
+                <label>
+                    <p>Compétences : </p>
+                    <textarea name="competence" row="8" placeholder="Renseignez vos compétences sous forme de mots clés. (Séparées par des points virgules (;))"></textarea>
+                </label>
+                <label>
+                    <p> Hobbies: </p>
+                    <textarea name="hobbies" row="8" placeholder="Des hobbies en particulier ? Ajoutez les ici ! (Séparées par des points virgules (;))"></textarea>
+                </label>
+                <input type="submit" value="Soumettre">
+            </form>
+        </diV>
+
+        <!-- <div id="div_file">
+            <h2>Via un fichier PDF</h2>
+            <form action="add.php" method="post" autocomplete="off" class="form-file">
+                <label>
+                    <p>Sélectionnez votre CV (Format PDF) : </p>
+                    <input type="file" name="pdf_file">
+                </label>
+                <input type="submit" value="Envoyer !">
+            </form>
+        </diV> -->
+    </diV>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -2,26 +2,108 @@
 
 require_once 'app/init.php';
 
-if (isset($_GET['q'])) {
-    $q = $_GET['q'];
-
+if (isset($_GET['global_search'])) {
+    $global_search = $_GET['global_search'];
     $query = $es->search([
         'body' => [
             'query' => [
                 'bool' => [
-                    'should' => [
-                        'match' => ['title' => $q],
-                        'match' => ['body' => $q],
-                        'match' => ['keywords' => $q]
+                    "should" => [
+                        [
+                            "match" => [
+                                "name" => $global_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "firstname" => $global_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "diplomas" => $global_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "experience" => $global_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "competence" => $global_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "hobbies" => $global_search
+                            ]
+                        ]
                     ]
                 ]
             ]
         ]
     ]);
+} else if (isset($_GET['identity_search'])) {
+    $identity_search = $_GET['identity_search'];
+    $query = $es->search([
+        'body' => [
+            'query' => [
+                'bool' => [
+                    "should" => [
+                        [
+                            "match" => [
+                                "name" => $identity_search
+                            ]
+                        ],
+                        [
+                            "match" => [
+                                "firstname" => $identity_search
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+}else if (isset($_GET['diplomas_search'])) {
+    $diplomas_search = $_GET['diplomas_search'];
+    $query = $es->search([
+        'body' => [
+            'query' => [
+                'bool' => [
+                    "should" => [
+                        [
+                            "match" => [
+                                "diplomas" => $diplomas_search
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+}else if (isset($_GET['competences_search'])) {
+    $competences_search = $_GET['competences_search'];
+    $query = $es->search([
+        'body' => [
+            'query' => [
+                'bool' => [
+                    "should" => [
+                        [
+                            "match" => [
+                                "competence" => $competences_search
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+}
 
-    if ($query['hits']['total'] >= 1) {
-        $results = $query['hits']['hits'];
-    }
+if (isset($query) && $query['hits']['total'] >= 1) {
+    $results = $query['hits']['hits'];
 }
 ?>
 
@@ -32,15 +114,36 @@ if (isset($_GET['q'])) {
     <meta charset="utf-8">
     <title>ES Projet</title>
 
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
     <form action="index.php" method="get" autocomplete="off">
         <label>
-            Rechercher quelque chose
-            <input type="text" name="q">
-            <input type="submit" value="Rechercher">
+            <p> Recherche Global : </p>
+            <input type="text" name="global_search">
+            <input type="submit" style="margin-left: 15px;" value="Rechercher">
+        </label>
+    </form>
+    <form action="index.php" method="get" autocomplete="off">
+        <label>
+            <p> Recherche sur l'identité : </p>
+            <input type="text" name="identity_search">
+            <input type="submit" style="margin-left: 15px;" value="Rechercher">
+        </label>
+    </form>
+    <form action="index.php" method="get" autocomplete="off">
+        <label>
+            <p> Recherche sur les diplômes : </p>
+            <input type="text" name="diplomas_search">
+            <input type="submit" style="margin-left: 15px;" value="Rechercher">
+        </label>
+    </form>
+    <form action="index.php" method="get" autocomplete="off">
+        <label>
+            <p> Recherche sur les compétences : </p>
+            <input type="text" name="competences_search">
+            <input type="submit" style="margin-left: 15px;" value="Rechercher">
         </label>
     </form>
     <?php
@@ -50,10 +153,12 @@ if (isset($_GET['q'])) {
             ?>
 
             <div class="result">
-                <a href="#<?php echo $r['_id']; ?>"><?php echo $r['_source']['title']; ?></a>
-                <div class="result-keywords"><?php echo implode(',', $r['_source']['keywords']); ?></div>
+                <a href="#<?php echo $r['_id']; ?>"><?php echo $r['_source']['name']; ?> <?php echo $r['_source']['firstname']; ?></a>
+                <div class="result-diplomas"><?php echo implode(',', $r['_source']['diplomas']); ?></div>
+                <div class="result-experience"><?php echo implode(',', $r['_source']['experience']); ?></div>
+                <div class="result-competence"><?php echo implode(',', $r['_source']['competence']); ?></div>
+                <div class="result-hobbies"><?php echo implode(',', $r['_source']['hobbies']); ?></div>
             </div>
-
         <?php
         }
     }
